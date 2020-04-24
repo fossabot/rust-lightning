916b9344c88cc7042d8ad08bc2263b7401c147b2
Matt Corallo <git@bluematt.me>
2020-04-24 14:22:26 -0400

    Run with mutagen on travis.
    Sadly our test coverage isn't very good and I had to hunt for
    functions to mutate where we fail tests on every mutation mutagen
    creates, but committing the framework is a start.

--- lightning/src/lib.rs
+++ lightning/src/lib.rs
@@ -21,8 +21,15 @@
 extern crate bitcoin;
 #[cfg(test)] extern crate rand;
 #[cfg(test)] extern crate hex;
+#[cfg(all(test, feature = "mutation_testing"))] extern crate mutagen;
 
 #[macro_use]
 pub mod util;
 pub mod chain;
 pub mod ln;
+
+#[cfg(all(
+		any(feature = "mutation_testing", feature = "fuzztarget"),
+		not(any(test, debug_assertions))
+		))]
+const ERR: () = "You should never be building with feature = mutation_testing or feature = fuzztarget! They are used to compile with broken code for testing only!";

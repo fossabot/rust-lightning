ab4c7e650439739eb7e7a1e9c6713d2ba5aafd9e
Matt Corallo <git@bluematt.me>
2020-05-12 14:54:12 -0400

    Add all the manually-generated bits for the c-bindings crate
    Including:
     * A script to automatically generate all the rest,
     * Cargo.toml and cbindgen.toml,
     * manually-written wrapper types for a few types

---
+++ genbindings.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Generate (and reasonably test) C bindings
+
+# First wipe all the existing C bindings:
+rm lightning-c-bindings/src/{ln,util,chain,routing}/*
+
+set -e
+# Then build and run the latest c-bindings-gen binary
+cd c-bindings-gen && cargo build && cd ..
+SRC="$(pwd)/lightning/src"
+OUT="$(pwd)/lightning-c-bindings/src"
+OUT_TEMPL="$(pwd)/lightning-c-bindings/src/c_types/derived.rs"
+OUT_F="$(pwd)/lightning-c-bindings/include/rust_types.h"
+OUT_CPP="$(pwd)/lightning-c-bindings/include/lightningpp.hpp"
+RUST_BACKTRACE=1 ./c-bindings-gen/target/debug/c-bindings-gen $SRC/ $OUT/ lightning $OUT_TEMPL $OUT_F $OUT_CPP
+
+# Now cd to lightning-c-bindings, build the generated bindings, and call cbindgen to build a C header file
+PATH="$PATH:~/.cargo/bin"
+cd lightning-c-bindings
+cargo build
+cbindgen -v --config cbindgen.toml -o include/lightning.h
+
+# cbindgen is relatively braindead when exporting typedefs -
+# it happily exports all our typedefs for private types, even with the
+# generics we specified in C mode! So we drop all those types manually here.
+sed -i 's/typedef LDKln.*Import.*LDKln.*;//g' include/lightning.h
+
+# Finally, sanity-check the generated C and C++ bindings with demo apps:
+
+# Naively run the C demo app:
+gcc -Wall -g -pthread demo.c ../target/debug/liblightning.a -ldl
+./a.out
+
+# And run the C++ demo app in valgrind to test memory model correctness
+# Note that there are a few leaks currently, so we don't fail the script for leaks,
+# but ideally we shouldn't add new leaks and once we fix the existing ones we should
+# fail in response to any memory leaks.
+g++ -Wall -g -pthread demo.cpp -L../target/debug/ -llightning -ldl
+LD_LIBRARY_PATH=../target/debug/ valgrind --memcheck:leak-check=full --show-leak-kinds=all ./a.out
+
+# Test a statically-linked C++ version, tracking the resulting binary size and runtime 
+# across debug, LTO, and cross-language LTO builds (using the same compiler each time).
+clang++ -Wall -pthread demo.cpp ../target/debug/liblightning.a -ldl
+./a.out
+echo " C++ Bin size and runtime w/o optimization:"
+ls -lha a.out
+time ./a.out > /dev/null
+
+# Now build with LTO on on both C++ and rust, but without cross-language LTO:
+cargo rustc -v --release -- -C lto
+clang++ -Wall -flto -O2 -pthread demo.cpp ../target/release/liblightning.a -ldl
+echo "C++ Bin size and runtime with only RL (LTO) optimized:"
+ls -lha a.out
+time ./a.out > /dev/null
+
+# Finally, test cross-language LTO. Note that this will fail if rustc and clang++
+# build against different versions of LLVM (eg when rustc is installed via rustup
+# or Ubuntu packages). This should work fine on Distros which do more involved
+# packaging than simply shipping the rustup binaries (eg Debian should Just Work
+# here).
+cargo rustc -v --release -- -C linker-plugin-lto -C lto -C link-arg=-fuse-ld=lld
+clang++ -Wall -flto -O2 -pthread demo.cpp ../target/release/liblightning.a -ldl
+echo "C++ Bin size and runtime with cross-language LTO:"
+ls -lha a.out
+time ./a.out > /dev/null
---
+++ lightning-c-bindings/demo.c
@@ -0,0 +1,84 @@
+#include "include/rust_types.h"
+#include "include/lightning.h"
+
+#include <assert.h>
+#include <stdio.h>
+
+void print_log(const void *this_arg, const char *record) {
+	printf("%s", record);
+}
+
+uint32_t get_fee(const void *this_arg, LDKConfirmationTarget target) {
+	if (target == LDKConfirmationTarget_Background) {
+		return 253;
+	} else {
+		return 507;
+	}
+}
+
+void broadcast_tx(const void *this_arg, LDKTransaction tx) {
+	//TODO
+}
+
+LDKCResult_NoneChannelMonitorUpdateErrZ add_channel_monitor(const void *this_arg, LDKOutPoint funding_txo, LDKChannelMonitor monitor) {
+	return CResult_NoneChannelMonitorUpdateErrZ_good();
+}
+LDKCResult_NoneChannelMonitorUpdateErrZ update_channel_monitor(const void *this_arg, LDKOutPoint funding_txo, LDKChannelMonitorUpdate monitor) {
+	return CResult_NoneChannelMonitorUpdateErrZ_good();
+}
+LDKCVec_HTLCUpdateZ monitors_pending_htlcs_updated(const void *this_arg) {
+	LDKCVec_HTLCUpdateZ empty_htlc_vec;
+	empty_htlc_vec.data = NULL;
+	empty_htlc_vec.datalen = 0;
+	return empty_htlc_vec;
+}
+
+int main() {
+	uint8_t node_seed[32];
+
+	LDKNetwork net = LDKNetwork_Bitcoin;
+
+	LDKLogger logger;
+	logger.this_arg = NULL;
+	logger.log = print_log;
+
+	LDKFeeEstimator fee_est;
+	fee_est.this_arg = NULL;
+	fee_est.get_est_sat_per_1000_weight = get_fee;
+
+	LDKManyChannelMonitor mon;
+	mon.this_arg = NULL;
+	mon.add_monitor = add_channel_monitor;
+	mon.update_monitor = update_channel_monitor;
+	mon.get_and_clear_pending_htlcs_updated = monitors_pending_htlcs_updated;
+
+	LDKBroadcasterInterface broadcast;
+	broadcast.this_arg = NULL;
+	broadcast.broadcast_transaction = broadcast_tx;
+
+	LDKKeysManager keys = KeysManager_new(&node_seed, net, 0, 0);
+	LDKKeysInterface keys_source = KeysManager_as_KeysInterface(&keys);
+
+	LDKUserConfig config = UserConfig_default();
+
+	LDKChannelManager cm = ChannelManager_new(net, fee_est, mon, broadcast, logger, keys_source, config, 0);
+
+	LDKCVec_ChannelDetailsZ channels = ChannelManager_list_channels(&cm);
+	assert((unsigned long)channels.data < 4096); // There's an offset, but it should still be an offset against null in the 0 page
+	assert(channels.datalen == 0);
+	CVec_ChannelDetailsZ_free(channels);
+
+	LDKEventsProvider prov = ChannelManager_as_EventsProvider(&cm);
+	LDKCVec_EventZ events = (prov.get_and_clear_pending_events)(prov.this_arg);
+	assert((unsigned long)events.data < 4096); // There's an offset, but it should still be an offset against null in the 0 page
+	assert(events.datalen == 0);
+
+	ChannelManager_free(cm);
+	KeysManager_free(keys);
+
+	// Check that passing empty vecs to rust doesn't blow it up:
+	LDKCVec_HTLCUpdateZ empty_htlc_vec;
+	empty_htlc_vec.data = NULL;
+	empty_htlc_vec.datalen = 0;
+	CVec_HTLCUpdateZ_free(empty_htlc_vec);
+}
